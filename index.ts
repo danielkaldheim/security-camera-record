d4533fb9d78ad2dbc4b82a7d2b6c390fd1211ccb
Daniel Rufus Kaldheim <daniel@kaldheim.org>
2020-04-28 11:14:41 +0200

    added elasticsearch

--- index.ts
+++ index.ts
@@ -2,13 +2,22 @@ import dotenv from 'dotenv';
 import express from 'express';
 import path from 'path';
 import * as fs from 'fs';
+import ES from '@elastic/elasticsearch';
+import { exception } from 'console';
+
 // initialize configuration
 dotenv.config();
+
+const zeroPad = (num: number, places: number) =>
+  String(num).padStart(places, '0');
+
+const client = new ES.Client({ node: process.env.ES_SERVER });
 
 const port = process.env.SERVER_PORT;
 const dataDir = process.env.DATA_DIR;
 
 const app = express();
+app.use(express.json());
 
 app.use('/data', express.static(dataDir));
 app.use('/css', express.static(__dirname + '/css'));
@@ -25,20 +34,119 @@ app.get('/files', (req, res) => {
   fs.readdirSync(dataDir).forEach(file => {
     if (/.*\.mp4$/.test(file)) {
       const stats = fs.statSync(dataDir + '/' + file);
-      const res = file.match(/(?<name>[\w]+)_(?<year>[\d]{4})-(?<month>[\d]{2})-(?<day>[\d]{2}):(?<time>[\d:\d:\d]+)/);
+      const res = file.match(
+        /(?<name>[\w]+)_(?<year>[\d]{4})-(?<month>[\d]{2})-(?<day>[\d]{2}):(?<time>[\d:\d:\d]+)/
+      );
       files.push({
         filePath: file,
-        date: new Date(res[2]+'-'+res[3]+'-'+res[4]+' '+res[5]),
+        date: new Date(res[2] + '-' + res[3] + '-' + res[4] + ' ' + res[5]),
         stats: {
           size: stats.size,
           atime: stats.atime,
           mtime: stats.mtime,
-          ctime: stats.ctime,
+          ctime: stats.ctime
         }
       });
     }
   });
   res.json(files);
+});
+
+app.post('/log', async (req, res) => {
+  if (!req.body || !req.body.from || !req.body.to) {
+    res.json({ error: true, message: 'Invalid date' });
+  }
+  const from = new Date(req.body.from);
+  from.setHours(from.getHours() - 2);
+  const to = new Date(req.body.to);
+  to.setHours(to.getHours() - 2);
+
+  // console.log(
+  //   from.getFullYear() +
+  //     '-' +
+  //     zeroPad(from.getMonth() + 1, 2) +
+  //     '-' +
+  //     zeroPad(from.getDate(), 2) +
+  //     'T' +
+  //     zeroPad(from.getHours(), 2) +
+  //     ':' +
+  //     zeroPad(from.getMinutes(), 2) +
+  //     ':' +
+  //     zeroPad(from.getSeconds(), 2) +
+  //     ' -> ' +
+  //     to.getFullYear() +
+  //     '-' +
+  //     zeroPad(to.getMonth() + 1, 2) +
+  //     '-' +
+  //     zeroPad(to.getDate(), 2) +
+  //     'T' +
+  //     zeroPad(to.getHours(), 2) +
+  //     ':' +
+  //     zeroPad(to.getMinutes(), 2) +
+  //     ':' +
+  //     zeroPad(to.getSeconds(), 2)
+  // );
+  try {
+    const result = await client.search({
+      index: 'hass-events-v3-*',
+      body: {
+        from: 0,
+        size: 1000,
+        query: {
+          bool: {
+            must: [
+              {
+                query_string: {
+                  query:
+                    '(binary_sensor.hoveddor) OR (binary_sensor.sportsbod_dor) OR (person.jeanette_kaldheim) OR (person.daniel_rufus_kaldheim) OR (input_boolean.is_home) OR (sensor.oregon_scientific_temperature) OR (alarm_control_panel.skeisvold_60_alarm)',
+                  fields: ['hass.entity_id']
+                }
+              }
+            ],
+            filter: [
+              {
+                range: {
+                  '@timestamp': {
+                    gte:
+                      from.getFullYear() +
+                      '-' +
+                      zeroPad(from.getMonth() + 1, 2) +
+                      '-' +
+                      zeroPad(from.getDate(), 2) +
+                      'T' +
+                      zeroPad(from.getHours(), 2) +
+                      ':' +
+                      zeroPad(from.getMinutes(), 2) +
+                      ':' +
+                      zeroPad(from.getSeconds(), 2), // '2020-04-27T10:46:06'
+                    lte:
+                      to.getFullYear() +
+                      '-' +
+                      zeroPad(to.getMonth() + 1, 2) +
+                      '-' +
+                      zeroPad(to.getDate(), 2) +
+                      'T' +
+                      zeroPad(to.getHours(), 2) +
+                      ':' +
+                      zeroPad(to.getMinutes(), 2) +
+                      ':' +
+                      zeroPad(to.getSeconds(), 2) // '2020-04-27T10:46:06'
+                  }
+                }
+              }
+            ]
+          }
+        }
+      }
+    });
+    if (result.statusCode == 200) {
+      res.json(result.body);
+    } else {
+      res.json(result);
+    }
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 app.listen(port, () => {
--- src/components/Files.vue
+++ src/components/Files.vue
@@ -1,46 +1,85 @@
 <template>
-  <div class="hello">
-    <div class="video-block">
+  <div>
+    <div
+      class="btn-group days"
+      role="group"
+      v-if="this.days.length > 0 && this.activeDay"
+    >
+      <button
+        type="button"
+        class="btn btn-outline-dark"
+        :class="{ active: isActiveDay(day) }"
+        v-for="(day, index) in days"
+        :key="index"
+        @click="setActiveDay(day)"
+      >
+        {{ day }}
+      </button>
+    </div>
+    <div class="video-block" v-if="isActiveDay('Today')">
       <h4>Now</h4>
       <div>
-        <img
-          src="http://10.1.4.91:8080/?action=stream"
-          width="640"
-          height="480"
-        />
+        <img src="http://10.1.4.91:8080/?action=stream" class="video-preview" />
       </div>
     </div>
-    <div
-      v-for="(file, index) in files"
-      :key="file.filePath"
-      class="video-block"
-    >
-      <h4>{{ printDate(file.date) }}</h4>
-      <div>
-        <video
-          width="640"
-          height="480"
-          controls
-          :id="'video-' + index"
-          @durationchange="durationChangeEventHandler($event, file)"
-        >
-          <source :src="'/data/' + encodeURI(file.filePath)" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+    <v-template v-for="(file, index) in files" :key="file.filePath">
+      <div class="video-block" v-if="shouldShowVideo(file)">
+        <h4>{{ printDate(file.date) }}</h4>
+        <div style="display:flex; align-content: space-around;">
+          <div style="margin-left: auto">
+            <video
+              class="video-preview"
+              controls
+              :id="'video-' + index"
+              @durationchange="durationChangeEventHandler($event, file)"
+            >
+              <source
+                :src="'/data/' + encodeURI(file.filePath)"
+                type="video/mp4"
+              />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+
+          <div
+            style="text-align: left; margin-right: auto; padding-left: 1rem; width: 320px;"
+          >
+            <h5>Events:</h5>
+            <v-template v-if="activePlayer.file == file">
+              <v-template v-for="(entity, index) in entities" :key="index">
+                <div>
+                  <strong>{{ entity.name }}</strong
+                  >: <span v-html="getLogItem(entity)" />
+                </div>
+                <ul v-if="entity.domain == 'person' || entity.domain == 'binary_sensor'">
+                  <li
+                    v-for="(log, index) in getLogChanges(entity)"
+                    :key="'changes-' + index"
+                  >
+                    <small><strong>{{ log.time }}</strong>: {{ log.value }}</small>
+                  </li>
+                </ul>
+              </v-template>
+            </v-template>
+          </div>
+        </div>
+        <div class="action-block">
+          <a :href="'/data/' + encodeURI(file.filePath)"
+            ><i class="fa fa-download" aria-hidden="true"></i> Download</a
+          >
+          <i>{{ prettyFilesize(file.stats.size) }}</i>
+        </div>
       </div>
-      <div class="action-block">
-        <a :href="'/data/' + encodeURI(file.filePath)"
-          ><i class="fa fa-download" aria-hidden="true"></i> Download</a
-        >
-        <i>{{ prettyFilesize(file.stats.size) }}</i>
-      </div>
-    </div>
+    </v-template>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue } from 'vue-property-decorator';
 import filesize from 'filesize';
+import moment from 'moment';
+
+moment.locale('nb');
 
 interface Video {
   filePath: string;
@@ -53,21 +92,91 @@ interface Video {
   };
 }
 
+interface Player {
+  file?: Video;
+  currentTime: number;
+  currentDate?: Date;
+  status?: string;
+}
+
+interface Entity {
+  id: string;
+  name: string;
+  domain: string;
+}
+
+interface LogItem {
+  file: string;
+  timestamp: Date;
+  time: string;
+  name: string;
+  domain: string;
+  entity: string;
+  value: string;
+}
+
+interface DataInterface {
+  files: Video[];
+  days: string[];
+  activeDay: string;
+  logs: LogItem[];
+  entities: Entity[];
+  activePlayer: Player;
+}
+
 const Files = Vue.extend({
   data() {
-    return {
-      files: []
+    const d: DataInterface = {
+      activeDay: 'Today',
+      files: [],
+      days: [],
+      logs: [],
+      entities: [],
+      activePlayer: {
+        file: undefined,
+        currentTime: 0,
+        currentDate: undefined,
+        status: undefined
+      }
     };
+    return d;
   },
   created() {
     this.fetchFiles();
   },
   methods: {
     durationChangeEventHandler($event: any, file: Video) {
+      $event.target.addEventListener('play', () => {
+        const date = new Date(file.date);
+        this.fetchLog(file, date);
+        this.activePlayer.file = file;
+        this.activePlayer.status = 'play';
+      });
       $event.target.addEventListener('timeupdate', (e: any) => {
         const currentTime = e.target.currentTime;
-        console.log(file.date, currentTime);
+        const date = new Date(file.date);
+        date.setSeconds(date.getSeconds() + currentTime);
+        this.activePlayer.file = file;
+        this.activePlayer.status = 'playing';
+        this.activePlayer.currentTime = currentTime;
+        this.activePlayer.currentDate = date;
       });
+    },
+    isActiveDay(day: string): boolean {
+      return day == this.activeDay;
+    },
+    setActiveDay(day: string) {
+      this.activeDay = day;
+    },
+    shouldShowVideo(file: Video) {
+      const date = new Date(file.date);
+      const today = new Date();
+      let dateString: string = this.dateString(date);
+      const todayString: string = this.dateString(today);
+      if (dateString == todayString) {
+        dateString = 'Today';
+      }
+      return dateString == this.activeDay;
     },
     fetchFiles() {
       this.$http.get('/files').then((res: any) => {
@@ -76,7 +185,26 @@ const Files = Vue.extend({
           const bDate = new Date(b.date);
           return aDate > bDate ? -1 : aDate < bDate ? 1 : 0;
         });
+        this.files.splice(0, 1);
+
+        this.files.forEach((item: Video) => {
+          const date = new Date(item.date);
+          const today = new Date();
+          let dateString: string = this.dateString(date);
+          const todayString: string = this.dateString(today);
+          if (dateString == todayString) {
+            dateString = 'Today';
+          }
+          if (this.days.indexOf(dateString) <= -1) {
+            this.days.push(dateString);
+          }
+        });
       });
+    },
+    dateString(date: Date): string {
+      return (
+        date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
+      );
     },
     printDate(dateStr: string) {
       const d = new Date(dateStr);
@@ -84,6 +212,120 @@ const Files = Vue.extend({
     },
     prettyFilesize(size: number) {
       return filesize(size);
+    },
+    getLogChanges(entity: Entity) {
+      let logElements: LogItem[] = this.logs.filter((log: LogItem) => {
+        if (log.entity == entity.id) {
+          return true;
+        }
+      }).sort((a: LogItem, b: LogItem) => {
+          const aDate = new Date(a.timestamp);
+          const bDate = new Date(b.timestamp);
+          return aDate < bDate ? -1 : aDate > bDate ? 1 : 0;
+        });
+
+      let prevValue = logElements[0].value;
+      const changes: LogItem[] = [];
+      for (const log of logElements) {
+        if (prevValue != log.value) {
+          changes.push(log);
+          prevValue = log.value;
+        }
+      }
+      return changes;
+    },
+    getLogItem(entity: Entity) {
+      let logElements: LogItem[] = this.logs
+        .filter((log: LogItem) => {
+          if (log.entity == entity.id) {
+            return true;
+          }
+        })
+        .sort((a: LogItem, b: LogItem) => {
+          const aDate = new Date(a.timestamp);
+          const bDate = new Date(b.timestamp);
+          return aDate > bDate ? -1 : aDate < bDate ? 1 : 0;
+        });
+
+      if (logElements.length > 0) {
+        const item = logElements.find((log: LogItem) => {
+          if (this.activePlayer.currentDate) {
+            if (log.timestamp <= this.activePlayer.currentDate) {
+              return true;
+            }
+          }
+        });
+        if (item) {
+          let result = item.value;
+          if (entity.domain == 'binary_sensor') {
+            result = item.value == '1' ? 'Åpen' : 'Lukket';
+          } else if (entity.domain == 'input_boolean') {
+            if (item.value == '1') {
+              result =
+                '<i class="fa fa-check-square-o" aria-hidden="true"></i>';
+            } else {
+              result = '<i class="fa fa-square-o" aria-hidden="true"></i>';
+            }
+          } else if (entity.domain == 'person') {
+            result = item.value == '1' ? 'Hjemme' : 'Borte';
+          } else if (entity.domain == 'sensor') {
+            result = parseFloat(item.value).toFixed(2);
+          }
+          return result + ' &ndash; <small><em>' + item.time + '</em></small>';
+        }
+      }
+      return 'N/A';
+    },
+    fetchLog(file: Video, timeStamp: Date) {
+      const from = new Date(timeStamp.getTime());
+      from.setMinutes(from.getMinutes() - 10);
+      const to = new Date(timeStamp.getTime());
+      to.setHours(to.getHours() + 1);
+
+      this.$http
+        .post('/log', {
+          from: from.toUTCString(),
+          to: to.toUTCString()
+        })
+        .then((res: any) => {
+          const logData: any = res.data;
+          if (logData.hits.total > 0) {
+            const hits = logData.hits.hits;
+            hits.forEach((hit: any) => {
+              const entity: Entity = {
+                id: hit['_source']['hass.entity_id'],
+                name: hit['_source']['hass.attributes']['friendly_name'],
+                domain: hit['_source']['hass.domain']
+              };
+              let found = false;
+              for (const ent of this.entities) {
+                if (ent.id == hit['_source']['hass.entity_id']) {
+                  found = true;
+                }
+              }
+              if (!found) {
+                this.entities.push(entity);
+              }
+              const time = new Date(hit['_source']['@timestamp']);
+              this.logs.push({
+                file: file.filePath,
+                timestamp: new Date(hit['_source']['@timestamp']),
+                time: moment(time).format('LTS'),
+                name: hit['_source']['hass.attributes']['friendly_name'],
+                domain: hit['_source']['hass.domain'],
+                entity: hit['_source']['hass.entity_id'],
+                value: hit['_source']['hass.value']
+              });
+            });
+            this.entities = this.entities
+              .sort((a: Entity, b: Entity) => {
+                return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
+              })
+              .sort((a: Entity, b: Entity) => {
+                return a.domain < b.domain ? -1 : a.domain > b.domain ? 1 : 0;
+              });
+          }
+        });
     }
   }
 });
@@ -98,9 +340,25 @@ export default Files;
 }
 .action-block {
   margin: 0 auto;
-  width: 640px;
+  max-width: 960px;
   display: flex;
   padding-bottom: 1rem;
   justify-content: space-between;
+}
+.video-preview {
+  object-fit: cover;
+  margin: 0 auto;
+  display: block;
+  max-width: 640px;
+  max-height: 480px;
+  width: 100%;
+  height: auto;
+  margin-bottom: 1rem;
+}
+.days {
+  display: block;
+  margin: 0.5rem auto;
+  max-width: 960px;
+  text-align: center;
 }
 </style>
